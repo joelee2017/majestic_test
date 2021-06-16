106148788b178a3e8d4ba11da3079c55d54c906e
joe <as>
2021-06-16 10:04:07 +0800

    註冊驗證，信箱、手機不可重覆

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 二、註冊頁、localdb
 
+三、Login、Register 畫面調整
+
+四、LoginModel、RegisterModel 建立
+
+五、註冊驗證
--- majestic_test01/majestic_test01/Controllers/AccountController.cs
+++ majestic_test01/majestic_test01/Controllers/AccountController.cs
@@ -1,10 +1,19 @@
-﻿using majestic_test01.Models;
+﻿using majestic_test01.Data;
+using majestic_test01.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace majestic_test01.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AccountContext _accountContext;
+
+        public AccountController(AccountContext accountContext)
+        {
+            _accountContext = accountContext;
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
@@ -38,7 +47,45 @@ namespace majestic_test01.Controllers
                 return View(model);
             }
 
+            var remil = ConfirmRepeatEmail(model.Email);
+            var rphone = ConfirmRepeatPhone(model.Phone);
+            
+            if (remil || rphone)
+            {             
+                return View(model);
+            }
+
+
+
             return View();
+        }
+
+        /// <summary>
+        /// 確認信箱是否重覆
+        /// </summary>        
+        private bool ConfirmRepeatEmail(string mail)
+        {
+            bool result = true;
+            // result = _accountContext.Accounts.Any(s => s.Email == mail);
+            if (result)
+            {
+                TempData["RepeatEmail"] = "信箱不可重覆";
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 確認手機是否重覆
+        /// </summary>        
+        private bool ConfirmRepeatPhone(int phone)
+        {
+            bool result = true;
+            // result = _accountContext.Accounts.Any(s => s.Phone == phone);
+            if (result)
+            {
+                TempData["RepeatPhone"] = "電話不可重覆";
+            }
+            return result;
         }
     }
 }
